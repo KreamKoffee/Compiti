2d223a72ca4fa31259bd3deb8ac60a6633dd08ce
KreamKoffee <alexlin3007@gmail.com>
2025-08-11 11:29:38 +0200

    es2_pag250

---
+++ compiti estate 2025/es2_pag250/src/main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        int num;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Inserisci un numero positivo inferiore a 10000: ");
+            num = scanner.nextInt();
+        } while (num <= 0 || num >= 10000);
+
+        // Conta i divisori
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+
+        // Crea il vettore dei divisori
+        int[] divisori = new int[count];
+        int idx = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                divisori[idx++] = i;
+            }
+        }
+
+        // Visualizza i divisori
+        System.out.print("Divisori di " + num + ": ");
+        for (int d : divisori) {
+            System.out.print(d + " ");
+        }
+    }
+}
