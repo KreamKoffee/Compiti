1e0e890b6048d860c04e06624a88c1e088b105aa
KreamKoffee <alexlin3007@gmail.com>
2025-09-05 14:31:29 +0200

    es10_pag376

---
+++ compiti estate 2025/es10_pag376/src/Libro/Libro.java
@@ -0,0 +1,67 @@
+package Libro;
+
+public class Libro {
+    private String ISBN;
+    private String titolo;
+    private String autore;
+    private boolean prestito;
+
+
+    Libro(String ISBN, String titolo, String autore){
+        this.ISBN=ISBN;
+        this.titolo=titolo;
+        this.autore=autore;
+    }
+    public String getISBN(){
+        return ISBN;
+    }
+
+    public String geTitolo() {
+        return titolo;
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public boolean prendi_in_presto() {
+        if(prestito==false) {
+            prestito= true;
+            return true;
+        }else {
+            prestito=true;
+            return false;
+        }
+    }
+
+    public boolean restituisci() {
+        if(prestito==true) {
+            prestito=false;
+            return false;
+        }else {
+
+            return true;
+        }
+    }
+
+    String to_String()
+    {
+
+        String S="ISBN: " +ISBN+ "Autore: " +autore+ "Titolo: " +titolo+ "Stato libro: ";
+
+        if(prestito==false) {
+            S += "il libro è in biblioteca";
+        }
+        else
+        {
+            S += "il libro ce l'ha qualcuno";
+        }
+        return S;
+    }
+    public boolean getPrestito() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+
+}
---
+++ compiti estate 2025/es10_pag376/src/Libro/Main.java
@@ -0,0 +1,24 @@
+package Libro;
+
+public class Main {
+    public static void main(String[] args) {
+        Libro libro1 = new Libro("1234567890", "Il Grande Gatsby", "F. Scott Fitzgerald");
+        System.out.println(libro1.to_String());
+
+        // Prova a prendere in prestito il libro
+        if (libro1.prendi_in_presto()) {
+            System.out.println("Hai preso in prestito il libro.");
+        } else {
+            System.out.println("Il libro è già in prestito.");
+        }
+        System.out.println(libro1.to_String());
+
+        // Prova a restituire il libro
+        if (!libro1.restituisci()) {
+            System.out.println("Hai restituito il libro.");
+        } else {
+            System.out.println("Il libro non era in prestito.");
+        }
+        System.out.println(libro1.to_String());
+    }
+}
